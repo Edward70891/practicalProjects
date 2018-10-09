005f3497e7e8a83904cc68b6c29754e89131f227
Edward Demkowicz-Duffy <erdd501@york.ac.uk>
2018-10-09 11:27:58 +0100

    Made the extensions task func recursive (untested)

--- week2/extension.py
+++ week2/extension.py
@@ -1,22 +1,29 @@
 def menu(options):
-    if not all(isinstance(x, str) for x in options):
-        raise ValueError("Non-string options were passed to menu()")
-    for i in range(len(options)):
-        print(str(i + 1) + ") " + str(options[i]))
-    while(True):
-        print()
-        print("Please select an option by putting it's number in:")
-        selection = int(input(">")) - 1
-        if selection > len(options):
-            print("That's an invalid selection!")
-        else:
-            return selection
+        if not all(isinstance(x, str) for x in options):
+            for option in options:
+                if not isinstance(option, str) and not isinstance(option, list):
+                    raise ValueError("Non-string option passed to menu!")
+        for i in range(len(options) - 1):
+            optionName = options[i+1]
+            if isinstance(options[i+1], list):
+                optionName = options[i+1][0]
+            print(str(i + 1) + ") " + optionName)
+        while(True):
+            print()
+            print("Please select an option by putting it's number in:")
+            selection = int(input(">"))
+            if selection >= len(options):
+                print("That's an invalid selection!")
+            elif isinstance(options[selection], list):
+                return [selection].append(menu(options[selection]))
+            else:
+                return selection
 
 # The main program
 again = True
 total = 0
 while (again):
-    products = [["DVD", "Blu-ray", "New Game", "Game"], [2.5, 3.5, 4, 2.5]]
+    products = [["Products", "DVD", "Blu-ray", "New Game", "Game"], [0, 2.5, 3.5, 4, 2.5]]
     selection = menu(products[0])
     price = products[1][selection]
     total += price
@@ -26,4 +33,4 @@ while (again):
     if goAgain.lower() == "n":
         again = False
 
-print("Okay, your total cost is " + total + " pounds!")
+print("Okay, your total cost is " + str(total) + " pounds!")
