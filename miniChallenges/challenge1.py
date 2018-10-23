121e7466856e651ecccdaaf816451fd1bdc28293
Edward Duffy, Arch Laptop <edward70891@gmail.com>
2018-10-23 18:25:54 +0100

    Commented a shit ton in the first mini challenge so I can ask for help

--- miniChallenges/challenge1.py
+++ miniChallenges/challenge1.py
@@ -16,14 +16,19 @@ def basic(num):
     return prime
 
 
+# Runs through all numbers between 2 and the square root
+# (rounded down if necessary) of the number and checks if they divide it
 def advanced(num):
     prime = True
-    for i in range(2, math.floor(num**(0.5))):
+    for i in range(2, math.floor(num**(0.5)) + 1):
         if num % i == 0:
             prime = False
     return prime
 
 
+# Calls the above, but checks if the number is in the primes dictionary
+# beforehand. If it is a prime but not in the dictionary, it is added to the
+# dictionary.
 def clever(num):
     if num in primesDict.keys():
         prime = True
@@ -34,18 +39,41 @@ def clever(num):
     return prime
 
 
+# Returns a list of all the prime factors of a number; duplicates included.
+# Note: recursive.
 def primeFactors(num: int) -> list:
+    # Ensure the number is an integer, this has the double effect of triggering.
+    # an exception if the parse fails.
     num = int(num)
+    # Checks if the number is a prime using clever(), and if it is
+    # returns it in a list by itself.
+    # Note: the base case.
     if clever(num):
+        print("Largest prime factor found at " + str(num) + ", terminating...")
         return [num]
-    for i in range(2, math.floor(num**(0.5))):
+    # Runs through all the numbers between 2 and the target's square root
+    # (inclusive), and selects the first one which is both a factor of the
+    # target and prime, using clever() again.
+    # Note: the recursive case.
+    for i in range(2, math.floor(num**(0.5)) + 1):
         if num % i == 0 and clever(i):
-            prior = primeFactors(num/i)
-            return [prior].append(i)
-    raise ValueError("primeFactors didn't find a prime factor!")
+            print("Found the lowest prime factor at " + str(i))
+            print("Calling prime factors with " + str(int(num/i)))
+            # Calls the whole function again on the number divided by our
+            # prime factor. For example, in the case of 100 we will find
+            # the prime factor of 2 and call the function again with 50.
+            prior = primeFactors(int(num/i))
+            print("primeFactors terminated with " + str(prior.append(i)))
+            # Return the total list returned by lower recursions appended with
+            # the prime factor this iteration found.
+            return prior.append(i)
 
 
-# The actual func
+# The actual function, calls primeFactors and sorts it's return into
+# a dictionary to eliminate duplicates without any hassle (using the
+# keys of the dictionary as the numbers) then multiplys them together
+# to get the product. The unique list is returned along with the
+# product.
 def olympian(num):
     product = 1
     factors = primeFactors(num)
@@ -59,4 +87,4 @@ def olympian(num):
 
 data = olympian(100)
 print("The product was " + str(data[0]) + ".")
-print("This was from the data: " + print(str(data[1])))
+print("This was from the unique factors: " + print(str(data[1])))
