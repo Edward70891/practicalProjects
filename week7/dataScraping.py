bbdf6ad36bc2054a371a3a1769407e0446b72b15
Edward Duffy, Arch Laptop <edward70891@gmail.com>
2018-11-06 14:27:11 +0000

    Further refactorings and comments

--- week7/dataScraping.py
+++ week7/dataScraping.py
@@ -1,10 +1,32 @@
 def scrapeData(fileName):
+    """
+    scrapeData(string) -> [list, list, ...]
+    Reads a CSV-style file and returns a crude, raw list of
+    the list of values on each line.
+    """
     data = []
     with open(fileName, "r") as toRead:
         for line in toRead.readlines():
+            line.replace("\n", "").replace(" ", "")
             if line.split()[0][0] not in "#":
-                data.append([getValue(x.replace("\n", "").replace(" ", "")) for x in line.split(",")])
+                data.append([getValue(x) for x in line.split(",")])
     return data
+
+
+def writeCSV(filename, data):
+    """
+    writeData(filename <- string, data <- [list, list, ...]) -> None
+    Takes a list of nested lists and (over)writes it to the
+    given file path.
+    """
+    with open(filename, "w") as outputFile:
+        for row in data:
+            writeRow = ""
+            for i in range(len(row)):
+                writeRow += str(row[i])
+                if i != len(row) - 1:
+                    writeRow += ","
+            outputFile.write(writeRow + "\n")
 
 
 def getValue(string):
--- week7/level2Question3.py
+++ week7/level2Question3.py
@@ -1,26 +1,15 @@
-from dataScraping import scrapeData
-
-
-def writeCSV(filename, data):
-    with open(filename, "w") as outputFile:
-        for row in data:
-            writeRow = ""
-            for i in range(len(row)):
-                writeRow += str(row[i])
-                if i != len(row) - 1:
-                    writeRow += ","
-            outputFile.write(writeRow + "\n")
+from dataScraping import scrapeData, writeCSV
 
 
 # Scrape the data from the text file into a large table
-data = scrapeData("aberporth_meteorological_data.txt")
+dataRaw = scrapeData("aberporth_meteorological_data.txt")
 
 
 # dataDict is composed of:
 # year : [[month 1's data], [month 2's data]]
-# note that the actual month number is dropped in [2:]
+# notice that the actual month number is dropped in [2:]
 dataDict = {}
-for row in data:
+for row in dataRaw:
     key = row[0]
     value = [row[2:]]
     newValue = []
@@ -28,11 +17,12 @@ for row in data:
     for o in range(len(value[0])):
         # Get all the values of the current type for that year, eg. all the temperatures, rainfalls, and store them in a list
         dataValues = [value[i][o] for i in range(len(value))]
-        # Calculate the average of that list and append it to the new list
-        newValue.append(sum(dataValues) / len(dataValues))
+        # Calculate the average of that list
+        avgValue = round(sum(dataValues) / len(dataValues), 3)
+        newValue.append(avgValue)
     dataDict[key] = newValue
 
-print(dataDict)
 # Get the data back in the right format, ie. [year, data1, data2, ...]
-data = [(list(key) + value) for key, value in dataDict.items()]
+data = [[key] + value for key, value in dataDict.items()]
 writeCSV("processedData.csv", data)
+print("Finished with no errors!")
